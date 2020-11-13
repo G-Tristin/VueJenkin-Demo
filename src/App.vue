4212cf0a8f0a8b4032ad0b05ad1a42ba946143b5
jyq <2547521393@qq.com>
2020-11-13 09:55:13 +0800

    测试jenkins

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App add test"/>
+    <HelloWorld msg="Welcome to Your Vue.js App add test10"/>
   </div>
 </template>
 
